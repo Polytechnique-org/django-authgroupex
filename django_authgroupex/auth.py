80e59040dfdd14bf6af15e794cc06061236b924e
Raphaël Barrois <raphael.barrois@polytechnique.org>
2013-02-02 01:43:12 +0100

    Fix auth.get_model.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- django_authgroupex/auth.py
+++ django_authgroupex/auth.py
@@ -36,7 +36,7 @@ def get_model(model_name):
     - Explicit models.Model subclass
     - Absolute dotted path to import the model
     """
-    if issubclass(model_name, models.Model):
+    if isinstance(model_name, type) and issubclass(models.Model, model_name):
         return model_name
 
     # Not a Model or a Model instance, must be a class path
