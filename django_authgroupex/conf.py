51296f17f618dffcb3685fc97936ee96299cf757
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2013-11-25 15:43:05 +0100

    Fix LOGIN_REDIRECT_URL handling.

--- django_authgroupex/conf.py
+++ django_authgroupex/conf.py
@@ -13,16 +13,24 @@ import appconf
 class AuthGroupeXConf(appconf.AppConf):
     """Global, django-appconf based config for authgroupex."""
 
+    class Meta:
+        prefix = 'authgroupex'
+
     # Pre-shared key for authenticating our site to remote
     KEY = ''
 
     # Url of the endpoint
     ENDPOINT = 'https://www.polytechnique.org/auth-groupex/utf8'
 
-    # Return URL where the AuthGroupeX server should user back
+    # Return URL where the AuthGroupeX server should send back the user-agent
     RETURN_URL = ''
     def configure_return_url(self, value):
         return value or settings.LOGIN_URL
+
+    # Redirection URL post-authgroupex if no next= view was set.
+    LOGIN_REDIRECT_URL = ''
+    def config_login_redirect_url(self, value):
+        return value or settings.LOGIN_REDIRECT_URL
 
     # Fields to fetch from remote; order matters.
     FIELDS = ('username', 'firstname', 'lastname', 'email')
@@ -33,17 +41,19 @@ class AuthGroupeXConf(appconf.AppConf):
     # The AuthGroupeX permissions that enable the 'is_staff' flag on this site
     STAFF_PERMS = ()
 
-    # Whether accounts for now-dead users should switch to is_active=False
-    DISABLE_DEADS = False
-
-    # Name of the group to query as
-    GROUP = ''
-
     # Maps a auth-groupex perm to a local group name
     MAP_GROUPS = {}
 
+    # Whether accounts for now-dead users should switch to is_active=False
+    DISABLE_DEADS = False
+
+    # Name of the 'GroupeX' to query ENDPOINT as
+    GROUP = ''
+
     # Model to store users to
-    USER_MODEL = 'auth.User'
+    USER_MODEL = ''
+    def config_user_model(self, value):
+        return value or getattr(settings, 'AUTH_USER_MODEL', '') or 'auth.User'
 
     # Group to store groups to
     GROUP_MODEL = 'auth.Group'
@@ -52,8 +62,3 @@ class AuthGroupeXConf(appconf.AppConf):
     FAKE = False
     def configure_fake(self, value):
         return value or settings.DEBUG
-
-    class Meta:
-        prefix = 'authgroupex'
-        # Forward setting lookups to base django.conf.settings
-        proxy = True
