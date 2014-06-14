33b9390f60284f8fc3b31df85479e9df930a91bf
Victor Berger <victor.berger@m4x.org>
2014-06-14 13:52:18 +0200

    Fix authgroupex_dev website.
    Signed-off-by: Victor Berger <victor.berger@m4x.org>

--- django_authgroupex_dev/urls.py
+++ django_authgroupex_dev/urls.py
@@ -6,9 +6,9 @@ admin.autodiscover()
 admin.site.login_template = 'authgroupex/admin_login.html'
 
 urlpatterns = patterns('',
-    url(r'^$', 'dev.devsite.views.home', name='home'),
+    url(r'^$', 'django_authgroupex_dev.devsite.views.home', name='home'),
     url(r'^xorgauth/', include('django_authgroupex.urls', namespace='authgroupex')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^logout/', 'dev.devsite.views.logout', name='logout'),
+    url(r'^logout/', 'django_authgroupex_dev.devsite.views.logout', name='logout'),
 )
